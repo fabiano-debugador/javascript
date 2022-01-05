ca519e767003d5bd7031320fee166e1011d2fbd9
fabiano-debugador <fabiano_the_beast@hotmail.com>
2022-01-04 23:22:08 -0300

    object entries

---
+++ object/entries.js
@@ -0,0 +1,9 @@
+const obj = {
+    nome: "james",
+    idade: "36"
+}
+
+const newObj = Object.entries(obj);
+
+// return a new array [ [ 'nome', 'james' ], [ 'idade', '36' ] ]
+console.log(newObj);
